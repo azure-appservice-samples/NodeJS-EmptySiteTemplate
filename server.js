bbcb7c1693d589bc10ae83ddba08dfa195aa839c
likaho <likaho@users.noreply.github.com>
2016-10-16 08:34:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-    
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
+            app.get('/webhook', function(req, res) {
+          if (req.query['hub.mode'] === 'subscribe' &&
+              req.query['hub.verify_token'] === 'carinaserver_token') {
+            console.log("Validating webhook");
+            res.status(200).send(req.query['hub.challenge']);
+          } else {
+            console.error("Failed validation. Make sure the validation tokens match.");
+            res.sendStatus(403);          
+          }  
+        });
+    //res.writeHead(200, { 'Content-Type': 'text/html' });
+    //res.end('Hello, world!');
     
 }).listen(process.env.PORT || 8080);
