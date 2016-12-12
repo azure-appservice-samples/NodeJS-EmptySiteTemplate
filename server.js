e2cace7a8547c2f252534058f48e4956952167fb
นาย วันเฉลิม เหลาเกตุ <ttvone@hotmail.com>
2016-12-12 14:02:11 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
+    res.end('Hello, world สวัสดีชากโลก!');
     
 }).listen(process.env.PORT || 8080);
