877994fda709723879ff2ffd22eccc7c3126345a
Todd Tkach <toddtkach@gmail.com>
2017-01-19 22:11:05 -0500

    Boilerplate Updates
    Init Boilerplate

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
-var http = require('http');
+var express = require("express");
+var app = express();
+var router = express.Router();
+var path = __dirname + '/views/';
 
-http.createServer(function (req, res) {
-    
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
-    
-}).listen(process.env.PORT || 8080);
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
+});
+
+router.get("/",function(req,res){
+  res.sendFile(path + "index.html");
+});
+
+router.get("/about",function(req,res){
+  res.sendFile(path + "about.html");
+});
+
+router.get("/contact",function(req,res){
+  res.sendFile(path + "contact.html");
+});
+
+app.use("/",router);
+
+app.use("*",function(req,res){
+  res.sendFile(path + "404.html");
+});
+
+var port = process.env.PORT || 3000;
+
+app.listen(port,function(){
+  console.log("Live at Port " + port);
+});
