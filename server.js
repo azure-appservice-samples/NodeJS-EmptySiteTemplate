57143a9bb087a36ce94da6f9567c9b2ddabd70e1
Roberto Luis Arreola Leal <35072803+roberto-arreola@users.noreply.github.com>
2018-12-04 21:43:14 -0600

    Update server.js
    Update site validation

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
+    res.end('Hello, Soft Hotel!');
     
 }).listen(process.env.PORT || 8080);
