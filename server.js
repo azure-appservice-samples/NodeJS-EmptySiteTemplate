8413daeb3403794d2169c4895b019cc175575633
Bandile P Dlamini <48508260+u14402425@users.noreply.github.com>
2019-03-14 23:47:55 +0200

    Change from Hello World to Project Title (Client Accounts System)
    Made this change to check if project can be edited from Github directly to the Azure Web Server

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
+    res.end('Client Accounts System');
     
 }).listen(process.env.PORT || 8080);
